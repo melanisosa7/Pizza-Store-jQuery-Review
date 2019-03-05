3e3e572499b07925e93e725af57608f34eaeb4bc
melanisosa7 <43611226+melanisosa7@users.noreply.github.com>
2019-03-05 09:29:58 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".pizza").click(function() {
+   $("#img1").show();
+});  
+$(".knots").click(function() {
+   $("#img2").show();
+});  
+$(".salad").click(function() {
+   $("#img3").show();
+});  
+$(".spaghetti").click(function() {
+   $("#img4").show();
+});  
